c3c60ed3a8a410679abc054f7d2e82636c29aab9
宇阳 <3311118881@qq.com>
2024-08-14 14:27:10 +0800

    完成rss解析接口

--- blog/src/main/java/liuyuyang/net/controller/RssController.java
+++ blog/src/main/java/liuyuyang/net/controller/RssController.java
@@ -1,16 +1,31 @@
 package liuyuyang.net.controller;
 
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.XmlReader;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import liuyuyang.net.model.Rss;
+import liuyuyang.net.result.Result;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
+@Api(tags = "订阅管理")
+@RestController
+@RequestMapping("/rss")
 public class RssController {
-    public static void main(String[] args) {
+    @GetMapping
+    @ApiOperation("获取订阅内容")
+    @ApiOperationSupport(author = "刘宇阳 | liuyuyang1024@yeah.net", order = 1)
+    public Result<List<Rss>> list() {
+        List<Rss> rssList = new ArrayList<>();
+
         // 目标网站列表
         List<String> feedUrls = Arrays.asList(
                 "https://blog.zwying.com/feed/",
@@ -29,17 +44,32 @@ public class RssController {
                 SyndFeed feed = input.build(xmlReader);
 
                 // 遍历提要中的条目
-                for (SyndEntry entry : feed.getEntries()) {
-                    System.out.println("Title: " + entry.getTitle());
-                    System.out.println("Link: " + entry.getLink());
-                    System.out.println("Published Date: " + entry.getPublishedDate());
-                    System.out.println("Description: " + entry.getDescription().getValue());
-                    System.out.println();
+                for (SyndEntry data : feed.getEntries()) {
+                    Rss rss = new Rss();
+                    rss.setAuthor(data.getAuthor());
+                    rss.setTitle(data.getTitle());
+                    rss.setDescription(data.getDescription().getValue());
+                    rss.setUrl(data.getLink());
+                    rss.setCreateTime(String.valueOf(data.getPublishedDate().getTime()));
+                    rssList.add(rss);
                 }
             } catch (Exception e) {
-                System.err.println("Error parsing feed: " + feedUrl);
+                System.err.println("解析失败: " + feedUrl);
                 e.printStackTrace();
             }
         }
+
+        // 对rssList进行排序，根据createTime降序排序
+        Collections.sort(rssList, new Comparator<Rss>() {
+            @Override
+            public int compare(Rss o1, Rss o2) {
+                long time1 = Long.parseLong(o1.getCreateTime());
+                long time2 = Long.parseLong(o2.getCreateTime());
+                // 降序排序
+                return Long.compare(time2, time1);
+            }
+        });
+
+        return Result.success(rssList);
     }
 }
