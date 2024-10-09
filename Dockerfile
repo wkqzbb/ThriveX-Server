a3abe946e85f5bedf37ceb7512f8a445c1d7b9ca
宇阳 <3311118881@qq.com>
2024-10-09 18:56:05 +0800

    。。

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /thrive
 COPY thrive.jar /thrive/app.jar
 
 # 暴露容器端口号，不写表示所有
-EXPOSE 9999
+EXPOSE 9003
 
 # 创建容器成功做的事情,等价于：java -jar app.jar
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- blog/src/main/java/liuyuyang/net/Main.java
+++ blog/src/main/java/liuyuyang/net/Main.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("服务已启动：http://localhost:9999/doc.html");
+        System.out.println("服务已启动");
         SpringApplication.run(Main.class);
     }
 }
