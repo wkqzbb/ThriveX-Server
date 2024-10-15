8b490134bccefbbcadad9ea4457170b942d0585a
宇阳 <3311118881@qq.com>
2024-10-15 18:36:26 +0800

    默认发件人

--- blog/src/main/java/liuyuyang/net/controller/EmailController.java
+++ blog/src/main/java/liuyuyang/net/controller/EmailController.java
@@ -4,11 +4,12 @@ import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import liuyuyang.net.annotation.NoTokenRequired;
+import liuyuyang.net.dto.email.CommentEmailDTO;
 import liuyuyang.net.utils.EmailUtils;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
 import javax.mail.MessagingException;
@@ -21,12 +22,27 @@ public class EmailController {
     @Resource
     private EmailUtils emailUtils;
 
-    @GetMapping
+    @Resource
+    private TemplateEngine templateEngine;
+
+    @PostMapping("/comment")
     @NoTokenRequired
-    @ApiOperation("发送邮件")
+    @ApiOperation("发送评论邮件")
     @ApiOperationSupport(author = "刘宇阳 | liuyuyang1024@yeah.net", order = 1)
-    public String test() throws MessagingException {
-        emailUtils.send("liuyuyang1024@yeah.net", "测试标题", "好久不见，我回来了！", "宇阳", "2024年10月15日 14:44", "希望明年能够实现月薪过万的目标，这也是我的目标 哈哈哈", "https://liuyuyang.net");
+    public String comment(@RequestBody CommentEmailDTO email) throws Exception {
+        // 处理邮件模板
+        Context context = new Context();
+        context.setVariable("title", email.getTitle());
+        context.setVariable("reviewers", email.getReviewers());
+        context.setVariable("time", email.getTime());
+        context.setVariable("content", email.getContent());
+        context.setVariable("url", email.getUrl());
+        String template = templateEngine.process("comment_email", context);
+
+        // emailUtils.send("liuyuyang1024@yeah.net", "测试标题", "好久不见，我回来了！", "宇阳", "2024年10月15日 14:44", "希望明年能够实现月薪过万的目标，这也是我的目标 哈哈哈", "https://liuyuyang.net");
+        System.out.println(email);
+        emailUtils.send(email.getTo() != null && !email.getTo().isEmpty() ? email.getTo() : null, email.getSubject(), template);
+
         return "发送邮件";
     }
 }
--- common/util/src/main/java/liuyuyang/net/utils/EmailUtils.java
+++ common/util/src/main/java/liuyuyang/net/utils/EmailUtils.java
@@ -4,39 +4,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
-import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 @Component
 public class EmailUtils {
     @Resource
     private JavaMailSender mailSender;
-    @Resource
-    private TemplateEngine templateEngine;
 
     @Value("${spring.mail.username}")
     private String from;
 
-    public void send(String to, String subject, String title, String reviewers, String time, String content, String url) throws MessagingException {
+    public void send(String to, String subject, String template) throws Exception {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
 
-        // 处理邮件模板
-        Context context = new Context();
-        context.setVariable("title", title);
-        context.setVariable("reviewers", reviewers);
-        context.setVariable("time", time);
-        context.setVariable("content", content);
-        context.setVariable("url", url);
-        String template = templateEngine.process("email", context);
-
         // 发送邮件
-        helper.setFrom(from); // 发送者
-        helper.setTo(to); // 接收者
+        helper.setFrom(new InternetAddress(from, "你有新的消息~")); // 发送者
+        helper.setTo(to != null && !to.isEmpty() ? to : from); // 接收者
         helper.setSubject(subject); // 邮件标题
         helper.setText(template, true); // 第二个参数为 true 表示发送 HTML 格式
         mailSender.send(message);
