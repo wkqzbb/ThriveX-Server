cbaaa4a5a75255f402cef3c3aa54f8f43fbf5a3f
Liu 宇阳 <liuyuyang1024@yeah.net>
2024-11-21 16:18:57 +0800

    新增：异步注解

--- blog/src/main/java/liuyuyang/net/Main.java
+++ blog/src/main/java/liuyuyang/net/Main.java
@@ -2,8 +2,10 @@ package liuyuyang.net;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Main {
     public static void main(String[] args) {
         System.out.println("服务已启动");
