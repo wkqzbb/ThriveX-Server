d9abcb8a9c03f40710fe607bb250b97a6f36bb91
宇阳 <liuyuyang1024@yeah.net>
2025-01-24 12:47:17 +0800

    小调整

---
+++ common/src/main/java/liuyuyang/net/aspect/PremIdAspect.java
@@ -0,0 +1,98 @@
+package liuyuyang.net.aspect;
+
+import io.jsonwebtoken.Claims;
+import liuyuyang.net.annotation.PremId;
+import liuyuyang.net.execption.CustomException;
+import liuyuyang.net.properties.JwtProperties;
+import liuyuyang.net.utils.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+@Slf4j
+@Aspect
+@Component
+public class PremIdAspect {
+    @Resource
+    private JwtProperties jwtProperties;
+
+    // 定义切点，支持类和方法上的注解
+    @Pointcut("@within(liuyuyang.net.annotation.PremId) || @annotation(liuyuyang.net.annotation.PremId)")
+    private void cut() {
+    }
+
+    @Before("cut()")
+    public void before(JoinPoint joinPoint) {
+        PremId id = getMethodAnnotation(joinPoint);
+
+        if (id != null) {
+            String prem = id.value();
+            log.info("权限名称：{}", prem);
+
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                HttpServletRequest request = attributes.getRequest();
+                HttpServletResponse response = attributes.getResponse();
+
+                // 获取token
+                String token = request.getHeader("Authorization");
+                System.out.println("Authorization Header: " + token);
+
+                if (token != null && token.startsWith("Bearer ")) {
+                    token = token.substring(7);
+                }
+
+                Map<String, Object> role;
+
+                // 解析token
+                try {
+                    Claims claims = JwtUtils.parseJWT(jwtProperties.getSecretKey(), token);
+                    System.out.println(claims);
+                    role = (Map<String, Object>) claims.get("roleId");
+                } catch (Exception e) {
+                    response.setStatus(401);
+                    throw new CustomException(401, "身份验证失败：无效或过期的token");
+                }
+
+                log.info("角色ID：{}", role.get("id"));
+            }
+        }
+    }
+
+    // 获取当前方法上的 @PermName 注解
+    private PremId getMethodAnnotation(JoinPoint joinPoint) {
+        Method method = getCurrentMethod(joinPoint);
+        if (method != null) {
+            return method.getAnnotation(PremId.class);
+        }
+        return null;
+    }
+
+    // 获取当前执行的方法对象
+    private Method getCurrentMethod(JoinPoint joinPoint) {
+        try {
+            String methodName = joinPoint.getSignature().getName();
+            Class<?> targetClass = joinPoint.getTarget().getClass();
+            Method[] methods = targetClass.getMethods();
+            for (Method method : methods) {
+                if (method.getName().equals(methodName)) {
+                    return method;
+                }
+            }
+        } catch (Exception e) {
+            log.error("获取方法时出错", e);
+        }
+        return null;
+    }
+}
---
+++ model/src/main/java/liuyuyang/net/model/Permission.java
@@ -0,0 +1,24 @@
+package liuyuyang.net.model;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+@TableName("permission")
+public class Permission {
+    @TableId(type = IdType.AUTO)
+    @ApiModelProperty(value = "权限id")
+    private Integer id;
+
+    @ApiModelProperty(value = "权限名称", example = "user:add", required = true)
+    private String name;
+
+    @ApiModelProperty(value = "权限描述", example = "新增用户", required = true)
+    private String description;
+
+    @ApiModelProperty(value = "权限分组", example = "user", required = true)
+    private String group;
+}
