6c9ffc5146a0c37a2de05b250e4df15f300d6219
宇阳 <liuyuyang1024@yeah.net>
2025-01-26 14:00:40 +0800

    优化部分代码

--- blog/src/main/java/liuyuyang/net/Main.java
+++ blog/src/main/java/liuyuyang/net/Main.java
@@ -5,12 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
 @EnableAsync
 @EnableFileStorage
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("服务已启动");
         SpringApplication.run(Main.class, args);
     }
 }
--- blog/src/main/java/liuyuyang/net/config/PrintConfig.java
+++ blog/src/main/java/liuyuyang/net/config/PrintConfig.java
@@ -1,5 +1,6 @@
 package liuyuyang.net.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -11,6 +12,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+@Slf4j
 @Component
 public class PrintConfig {
     private static final Logger logger = LoggerFactory.getLogger(PrintConfig.class);
@@ -18,8 +20,8 @@ public class PrintConfig {
     private final AtomicBoolean printed = new AtomicBoolean(false);
 
     private static final Map<String, String> CONFIG_MAPPINGS = new LinkedHashMap<>();
+
     static {
-        CONFIG_MAPPINGS.put("storage.domain", "DOMAIN");
         CONFIG_MAPPINGS.put("server.port", "PORT");
         CONFIG_MAPPINGS.put("spring.datasource.url", "DB_INFO");
         CONFIG_MAPPINGS.put("spring.datasource.username", "DB_USERNAME");
@@ -28,9 +30,6 @@ public class PrintConfig {
         CONFIG_MAPPINGS.put("lyy.email.port", "EMAIL_PORT");
         CONFIG_MAPPINGS.put("lyy.email.username", "EMAIL_USERNAME");
         CONFIG_MAPPINGS.put("lyy.email.password", "EMAIL_PASSWORD");
-        CONFIG_MAPPINGS.put("lyy.oss.accessKey", "OSS_ACCESS_KEY");
-        CONFIG_MAPPINGS.put("lyy.oss.secretKey", "OSS_SECRET_KEY");
-        CONFIG_MAPPINGS.put("lyy.oss.bucket", "OSS_BUCKET");
     }
 
     public PrintConfig(ConfigurableEnvironment environment) {
@@ -39,14 +38,22 @@ public class PrintConfig {
 
     @EventListener
     public void printConfigurations(ApplicationStartedEvent event) {
+         log.info("\n----------------------------------------------------------\n\t\t" +
+                        "服务已启动: 欢迎使用 ThriveX 博客管理系统 \n\t\t" +
+                        "接口地址: \thttp://localhost:{}/api\n\t\t" +
+                        "API文档: \thttp://localhost:{}/doc.html\n\t\t" +
+                        "加入项目交流群: liuyuyang2023\n" +
+                        "----------------------------------------------------------",
+                environment.getProperty("server.port"), environment.getProperty("server.port"));
+
         if (printed.compareAndSet(false, true)) {
             logger.info("=== 配置信息 ===");
-            
+
             CONFIG_MAPPINGS.forEach((configPath, envVar) -> {
                 String value = environment.getProperty(configPath);
                 logger.info("{} = {}", envVar, value);
             });
-                    
+
             logger.info("=== 配置信息打印完成 ===");
         }
     }
