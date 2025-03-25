583c7559604662e9852219fd25df3d3de0404474
坐公交也用券 <liumou.site@qq.com>
2025-03-25 16:24:13 +0800

    增加信息打印

--- RUN.sh
+++ RUN.sh
@@ -17,6 +17,19 @@ if [ -z "${DB_PASSWORD}" ]; then
   echo "ERROR: Please set the DB_PASSWORD environment variable."
   exit 1
 fi
+# 信息打印
+echo "PORT: ${PORT}"
+echo "DB_INFO: ${DB_INFO}"
+echo "DB_USERNAME: ${DB_USERNAME}"
+#echo "DB_PASSWORD: ${DB_PASSWORD}"
+echo "EMAIL_HOST: ${EMAIL_HOST}"
+echo "EMAIL_PORT: ${EMAIL_PORT}"
+echo "EMAIL_USERNAME: ${EMAIL_USERNAME}"
+#echo "EMAIL_PASSWORD: ${EMAIL_PASSWORD}"
+## JAVA版本
+java -version
+## 程序版本
+echo "VERSION: ${VERSION}"
 cmd="java -jar /server/app.jar --PORT=${PORT} --DB_INFO=${DB_INFO} --DB_USERNAME=${DB_USERNAME} --DB_PASSWORD=${DB_PASSWORD} --EMAIL_HOST=${EMAIL_HOST} --EMAIL_PORT=${EMAIL_PORT} --EMAIL_USERNAME=${EMAIL_USERNAME} --EMAIL_PASSWORD=${EMAIL_PASSWORD}"
 echo "Running: $cmd"
 eval $cmd
