564bf9c6c05fb6aade21cbae1c7f3dea104898ca
神秘人 <3311118881@qq.com>
2025-04-27 13:20:43 +0800

    限制其他域名访问该服务

--- blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
+++ blog/src/main/java/liuyuyang/net/common/config/CorsConfig.java
@@ -1,19 +1,31 @@
 package liuyuyang.net.common.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    // @Value("${cors.allowed-origins}")
+    // private String[] allowedOrigins;
+
+    private String[] allowedOrigins = {
+        "http://localhost:3000",
+        "https://liuyuyang.net",
+        "https://www.liuyuyang.net",
+        "https://blog.liuyuyang.net"
+    };
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println(registry);
+
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedOriginPatterns("*")
-                // .allowedOriginPatterns(domainWhitelist)
-                // .allowedOriginPatterns("http://*.liuyuyang.net", "https://*.liuyuyang.net")
-                .allowedMethods("*") // 支持方法
-                .allowedHeaders("*");
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
